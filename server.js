4479415be7bfca7fa3bbf2953750aca3096bf1fb
Dempsey FP <dempseyfinley@gmail.com>
2021-02-04 16:44:09 -0500

    ready to run!

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('client/build'));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mern-starter",
@@ -32,6 +35,10 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, 'client/build/index.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
