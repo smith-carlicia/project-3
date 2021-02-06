abd271ae74572696d94deb7506aed1216ce76fb4
Adebambo Adeshiyan <badeshiyan@gmail.com>
2021-02-06 11:01:16 -0500

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mern-starter",
@@ -40,9 +40,9 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname, 'client/build/index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
