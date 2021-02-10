b66b33486a9a61032c3e2551dbe6a53966f8e7db
Carlicia Smith <smith.carlicia@gmail.com>
2021-02-10 14:57:30 -0500

    created input fields, submit button and newly rendered items container

--- client/src/containers/AddNewItem/AddNewItem.jsx
+++ client/src/containers/AddNewItem/AddNewItem.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import './AddNewItem.css';
+// import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+
 
 const AddNewItem = () => {
-  return <div className="container">"Add New Item Page"</div>;
+
+  return (
+   <Grid container direction="column" spacing={6}>
+     <h2 className="productManagerHeader">Admin Product Manager</h2>
+    <Grid item xs={6}>
+      <TextField
+        className="newItemName"
+        id="outlined-multiline-static"
+        label="Product Name"
+        rows={6}
+        defaultValue="Default Value"
+        variant="outlined"
+      />
+    </Grid>
+      <Grid item xs={6}>
+        <TextField
+          className="newItemDescription"
+          id="outlined-multiline-static"
+          label="Product Description"
+          rows={6}
+          defaultValue="Default Value"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={6}>
+      <TextField
+          className="newItemPrice"
+          id="outlined-multiline-static"
+          label="Product Price"
+          rows={4}
+          defaultValue="Default Value"
+          variant="outlined"
+        />
+      </Grid>
+        <Grid item xs={6}>
+          <TextField
+            className="newItemBrand"
+            id="outlined-multiline-static"
+            label="Product Brand"
+            rows={4}
+            defaultValue="Default Value"
+            variant="outlined"
+          />
+        </Grid>
+        {/* refactor onclick event to render newly added items in item manager div */}
+        <Button onClick={() => { alert('clicked') }} className="newItemSubmit" justify="center" variant="outlined" color="primary">
+          Submit
+        </Button>
+          {/* Submitted new products rendered here */}
+    <Grid item spacing={6} xs={5}></Grid>
+    <Grid item xs={7} direction="row" >
+      <Paper className="renderedNewItems">
+        Newly Submitted Products Rendered Here
+      </Paper>
+    </Grid>
+   </Grid>
+  )
 };
 
 export default AddNewItem;
