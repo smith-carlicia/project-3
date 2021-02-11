33566c7347d157169270aaa1c45bbef6c243d191
Dempsey FP <dempseyfinley@gmail.com>
2021-02-10 20:17:04 -0500

    destructed material cores imports

--- client/src/containers/SignUp/SignUp.jsx
+++ client/src/containers/SignUp/SignUp.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import './SignUp.css';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container }  from '@material-ui/core';
 import ChildFriendlyIcon from '@material-ui/icons/ChildFriendly';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 
 function Copyright() {
   return (
