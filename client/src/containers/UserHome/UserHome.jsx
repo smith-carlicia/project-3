469f491fb264d9f6a2fb21265fc9085271c1254a
Dempsey FP <dempseyfinley@gmail.com>
2021-02-11 23:00:42 -0500

    adding searchbar to userhome

--- client/src/containers/UserHome/UserHome.jsx
+++ client/src/containers/UserHome/UserHome.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, List, ListItem, ListItemAvatar, ListItemSecondaryAction, Grid, ListItemText, Typography, IconButton, Container } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { Avatar, List, ListItem, InputBase, ListItemAvatar, ListItemSecondaryAction, Grid, ListItemText, Typography, IconButton, Container } from '@material-ui/core';
 import { Add, Remove } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
 import productAPI from '../../utils/API_product';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+
         flexGrow: 1,
         maxWidth: 752,
     },
@@ -15,6 +17,45 @@ const useStyles = makeStyles((theme) => ({
     title: {
         margin: theme.spacing(4, 0, 2),
     },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      },
+      searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+      },
 }));
 
 // function generate(element) {
@@ -42,6 +83,20 @@ export default function UserHome() {
     }, []);
 
     return (
+        <>
+        <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </div>
         <div className={classes.root}>
             <Container fluid maxWidth="lg">
                 <Grid container>
@@ -75,5 +130,6 @@ export default function UserHome() {
                 </Grid>
             </Container>
         </div>
+        </>
     );
 }
