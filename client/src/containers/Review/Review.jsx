735ffbef02d0136cec618b5a90cc73aad48ce14f
Carlicia Smith <smith.carlicia@gmail.com>
2021-02-17 22:04:55 -0500

    relocated address and payment info states to checkout and refactored textfield values

--- client/src/containers/Review/Review.jsx
+++ client/src/containers/Review/Review.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Review.css';
+import Address from '../AddressForm/AddressForm';
+import Payment from '../PaymentForm/PaymentForm';
 import { Paper, Grid } from '@material-ui/core';
 
 const Review = () => {
 
+    // const addressInfo = Address;
+    // const paymentInfo = Payment;
+    // const listItems = 
+
     return(
         <Grid container>
             <Grid item>
-                <Paper  className="paper" elevation={0} />
+                <Paper  className="paper" elevation={0}>
+                    <Grid item>
+                        <h5>Products</h5>
+                    </Grid>
+                    <Grid item>
+                        <h5>Shipping</h5>
+                    </Grid>
+                    <Grid item>
+                        <h5>Billing</h5>
+                    </Grid>
+                </Paper>
             </Grid>
         </Grid>
     )
