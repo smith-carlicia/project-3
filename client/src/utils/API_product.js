d44c34c024686802fadf70de3fcbd3a7a589dfa5
Dempsey FP <dempseyfinley@gmail.com>
2021-02-20 00:51:09 -0500

    gj

--- client/src/utils/API_product.js
+++ client/src/utils/API_product.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 const productAPI = {
   findAllProducts: function () {
     return axios.get("/api/product");
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const path = require("path");
 const productRoutes = require("./product");
 // const userRoutes = require("./user");
 
